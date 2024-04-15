1f7926e3e07b9cb87903c73277f2f1f8a9c95a7d
Artur Migacz <75041222+Atyxon@users.noreply.github.com>
2024-04-15 22:18:05 +0200

    added main page html, configured static elements, configured sqlite database

--- warehouseApp/views.py
+++ warehouseApp/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 
 def main_view(request):
-    return HttpResponse("Witaj, jestem widokiem na domyślnym adresie!")
+    return render(request, 'index.html')
--- warehouse_inventory/urls.py
+++ warehouse_inventory/urls.py
@@ -17,8 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from warehouseApp import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('', views.main_view, name='main_view'),
     path('admin/', admin.site.urls),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
